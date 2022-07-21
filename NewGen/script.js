eb965545b427b3bba8c427ebf158866594418bbf
alex <kibardin2000@mail.ru>
2022-07-21 16:15:46 +0300

    add test item

---
+++ NewGen/script.js
@@ -0,0 +1,32 @@
+class UserService {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+        this.data = 'login: ' + this.username + 'password: ' + this.password;
+    }
+
+
+    authenticate_user(){
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://examples.com/api/user/authenticate?username=');
+        http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        xhr.send(this.data)
+        let result = false;
+        
+        if(xhr.status == 200 && xhr.readyState == 4 ){
+            result = true;
+        }  else{
+            result = xhr.response;
+            console.log(xhr.status)
+        }
+        return result;
+    }
+}
+const log = document.querySelector('#login'),
+user = document.querySelector('#username'),
+pass = document.querySelector('#password');
+
+log.addEventListener(click, ()=>{
+UserService.authenticate_user(user, pass);
+if(res == true) document.location.href = '/home'; else alert(res.error);
+});
